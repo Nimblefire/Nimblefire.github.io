628459152275a008c08710561e732db9eeff9be8
Nimblefire <dap18001@byui.edu>
2019-02-18 12:59:17 +0100

    modified js to include date

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,7 +1,19 @@
 
 function printCurrentDate() {
-    var dt = new Date();
-    document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+    var n =  new Date();
+    var y = n.getFullYear();
+    var m = n.getMonth() + 1;
+    var d = n.getDate();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var currentWeekday = weekday[d.getDay()];
+    document.getElementById("date").innerHTML =currentWeekday + ", " + d + m + y;
 }
 
 function toggleMenu(){
