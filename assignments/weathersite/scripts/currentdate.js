77c8f4400878ec8b8a321607b127e8e644af01da
Nimblefire <dap18001@byui.edu>
2019-03-23 12:49:40 +0100

    migrated website to weathersite

---
+++ assignments/weathersite/scripts/currentdate.js
@@ -0,0 +1,36 @@
+
+function printCurrentDate() {
+    var n =  new Date();
+    var y = n.getFullYear();
+    var d = n.getDate();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var monthName = new Array(12);
+    monthName[0] = "January";
+    monthName[1] = "February";
+    monthName[2] = "March";
+    monthName[3] = "April";
+    monthName[4] = "May";
+    monthName[5] = "June";
+    monthName[6] = "July";
+    monthName[7] = "August";
+    monthName[8] = "September";
+    monthName[9] = "October";
+    monthName[10] = "November";
+    monthName[11] = "December";
+
+    var currentMonth = monthName[n.getMonth()+1];
+    var currentWeekday = weekday[n.getDay()];
+    document.getElementById("datetime").innerHTML =currentWeekday + ", " + d + " " + currentMonth + " " + y;
+}
+
+function toggleMenu(){
+    document.getElementById("navigationTab").classList.toggle("hide");
+}
---
+++ assignments/weathersite/scripts/getData.js
@@ -0,0 +1,79 @@
+function getRequest() {
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ef215a532f3986dca09865df45262239';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var weatherjson = request.response;
+        var currently = document.querySelector('#value1');
+        var high = document.querySelector('#value2');
+        var windchill = document.querySelector('#value3');
+        var humidity = document.querySelector('#value4');
+        var windspeed = document.querySelector('#value5');
+        var temperatureF = Math.round((weatherjson.main.temp_max-273.15)*9/5 + 32);
+        currently.textContent = weatherjson.weather[0].main;
+        high.textContent = temperatureF;
+        humidity.textContent = weatherjson.main.humidity;
+        windspeed.textContent = weatherjson.wind.speed;
+        windchill.textContent = Math.round(35.74 + 0.6215*temperatureF - 35.75*Math.pow(weatherjson.wind.speed, 0.16) + 0.4275*temperatureF*Math.pow(weatherjson.wind.speed, 0.16));
+    }
+}
+
+function daysTemperatures() {
+    var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ef215a532f3986dca09865df45262239';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var forecastjson = request.response;
+        populateForecast(forecastjson);
+    }
+}
+
+function populateForecast(jsonObj){
+    var wrapper = document.querySelector('.wrapper');
+    var forecast_wrapper = document.querySelector('.forecast-wrapper');
+    var weekday = new Array(7);
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    for (var i=0; i < jsonObj.list.length; i++) {
+        if (jsonObj.list[i].dt_txt.includes('18:00:00')){
+            var dayDiv = document.createElement('div');
+            var date = new Date(jsonObj.list[i].dt_txt);
+            var currentWeekday = weekday[date.getDay()];
+
+            var imgDiv = document.createElement('div');
+            var img = document.createElement('img');
+
+            dayDiv.textContent = currentWeekday;
+            dayDiv.classList.add('days', 'day' + i);
+
+            img.src = 'https://openweathermap.org/img/w/' + jsonObj.list[i].weather[0].icon + '.png';
+            img.alt = jsonObj.list[i].weather[0].main;
+            imgDiv.classList.add('forecast-image', 'img'+i);
+
+            imgDiv.appendChild(img);
+            forecast_wrapper.appendChild(imgDiv);
+            wrapper.appendChild(dayDiv);
+        }
+    }
+
+    for (var i=0; i < jsonObj.list.length; i++) {
+        if (jsonObj.list[i].dt_txt.includes('18:00:00')){
+            var temperature = document.createElement('div');
+
+            temperature.textContent = Math.round((jsonObj.list[i].main.temp - 273.15)*9/5 + 32) + '°F';
+            temperature.classList.add('temp', 'temperature'+i)
+
+            forecast_wrapper.appendChild(temperature);
+        }
+    }
+}
